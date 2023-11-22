c077c1373a3a9b9a2c9d3a274a48077fd2f1c234
Zhaoyang Huang(local_pc) <554598474@qq.com>
2023-11-22 09:46:26 +0800

    pip包依赖更新

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
         name="VeloVGI",
-        version="0.0.3",
+        version="0.0.4",
         description="Graph Variational Autoencoder for scRNA-seq velocity",
         author="HuangDDU",
         author_email="hzy554598474@163.com",
@@ -20,7 +20,7 @@ setup(
             "torch-geometric==2.0.1",
             "scvi-tools==0.19.0",
             "scanpy==1.9.3",
-            "scvi-tools==0.19.0",
+            # "scvi-tools==0.19.0", Need to be installed manually because of version conflicts with velovi packages
             "scvelo==0.2.5",
             "velovi==0.2.0"
         ]
